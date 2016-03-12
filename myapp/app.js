b7f75401ccfdfca723845adc54b8850cadb7de30
divamela <divamhurtado@gmail.com>
2016-03-11 19:43:46 -0800

    Backend Addition
    adding express and connecting assets to the backend

---
+++ myapp/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.engine('html', require('ejs').renderFile);
+
+
+
+app.get('/', function (req, res) {
+  res.render('index.html', {name:"Bed"});
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
